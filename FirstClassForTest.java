e8aaab4db1c1f8189ab7aa8242dbaad6a43e2734
e.rafieepour <e.rafieepour@gmail.com>
2024-01-01 12:08:47 +0330

    Remove ; for Test Sync

--- FirstClassForTest.java
+++ FirstClassForTest.java
@@ -1,6 +1,6 @@
 public class FirstClassForTest {
     public static void main(String[] args){
-        System.out.println("Hello Fan apCamps!!");
+        System.out.println("Hello Fan apCamps!!")
 
     }
 }
