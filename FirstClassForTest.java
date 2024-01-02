8c757703d4340fe9044a91c4c990f648ff20653f
e.rafieepour <e.rafieepour@gmail.com>
2024-01-02 15:56:39 +0330

    Second Commit

--- FirstClassForTest.java
+++ FirstClassForTest.java
@@ -1,5 +1,10 @@
 public class FirstClassForTest {
     public static void main(String[] args){
         System.out.println("Hello Fan apCamps!!");
+        int a=2;
+        int b= -a;
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+
     }
 }
