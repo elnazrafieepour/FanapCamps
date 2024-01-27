1efbe70386dcc21f782277d26af69c292f1c3f42
admin <e.rafieepour@gmail.com>
2024-01-27 08:58:43 +0330

    to Add TowerOfHanoi.class for excersice of FanapCamps

---
+++ TowerOfHanoi.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int inputNum= input.nextInt();
+        System.out.println("Moves for Tower of Hanoi with " + inputNum + " disks:");
+        char sourceRod = 'A';
+        char auxiliaryRod = 'B';
+        char destinationRod = 'C';
+
+        input.close();
+
+
+        solveTowerOfHanoi(inputNum, sourceRod, auxiliaryRod, destinationRod);
+    }
+
+    public static void solveTowerOfHanoi(int n, char sourceRod, char auxiliaryRod, char destinationRod) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + sourceRod + " to " + destinationRod);
+            return;
+        }
+
+        solveTowerOfHanoi(n - 1, sourceRod, destinationRod, auxiliaryRod);
+        System.out.println("Move disk " + n + " from " + sourceRod + " to " + destinationRod);
+        solveTowerOfHanoi(n - 1, auxiliaryRod, sourceRod, destinationRod);
+    }
+}
