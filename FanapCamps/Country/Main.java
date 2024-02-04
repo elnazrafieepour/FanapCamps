2989762f8e4b20f652af040cd2194fc0722b6e4b
admin <e.rafieepour@gmail.com>
2024-02-04 18:26:47 +0330

    Forth Practice of 7 Meeting about Country Example and use of HashMap...

--- FanapCamps/Country/Main.java
+++ FanapCamps/Country/Main.java
@@ -1,14 +1,16 @@
 package FanapCamps.Country;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Country newCountry1= new Country("Iran", "IRN", 8500);
-        Country newCountry2= new Country("America", "USA", 1000);
-        Country newCountry3= new Country("Norway", "NOR", 9100);
-        Country newCountry4= new Country("Austria", "AUT", 750);
-        Country newCountry5= new Country("Australia", "AUS", 10);
+        Country newCountry1 = new Country("Iran", "IRN", 8500);
+        Country newCountry2 = new Country("America", "USA", 1000);
+        Country newCountry3 = new Country("Norway", "NOR", 9100);
+        Country newCountry4 = new Country("Austria", "AUT", 750);
+        Country newCountry5 = new Country("Australia", "AUS", 10);
 
         HashMap<String, Country> newMap = new HashMap<>();
         newMap.put(newCountry1.getSwiftCode(), newCountry1);
@@ -17,13 +19,14 @@ public class Main {
         newMap.put(newCountry4.getSwiftCode(), newCountry4);
         newMap.put(newCountry5.getSwiftCode(), newCountry5);
 
-        for (String e : newMap) {
-            System.out.println(newMap.toString());
+        for (Object entry : newMap.entrySet()) {
+            //String key = entry.getKey();
+            //String value = entry.getValue();
+            //System.out.println("Key: " + key + ", Value: " + value);
+            System.out.println(entry.toString());
         }
-*/
+
         System.out.println(newMap.get("AUT"));
         System.out.println(newMap.get("BLZ"));
-
-
     }
 }
