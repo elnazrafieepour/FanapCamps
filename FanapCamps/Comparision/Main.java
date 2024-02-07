0a0248c202bddc12a5cd5b49a7ebb44fdaf690f1
admin <e.rafieepour@gmail.com>
2024-02-07 16:31:39 +0330

    Second Practice of 7 Meeting about Add2List together and use of List...

---
+++ FanapCamps/Comparision/Main.java
@@ -0,0 +1,35 @@
+package FanapCamps.Comparision;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Main implements comparison2Array{
+    public static void main(String[] args) {
+        ArrayList<Integer> myList1 = new ArrayList<>();
+        ArrayList<Integer> myList2 = new ArrayList<>();
+
+        myList1.add(1);
+        myList1.add(2);
+        myList1.add(3);
+        myList1.add(4);
+        myList1.add(5);
+        myList1.add(0);
+        myList1.add(0);
+        myList1.add(6);
+        myList1.add(7);
+
+        myList2.add(1);
+        myList2.add(2);
+        myList2.add(4);
+        myList2.add(6);
+        myList2.add(7);
+        myList2.add(4);
+
+        ArrayList<Integer> finalArray= comparison2Array.compare(myList1, myList2);
+        System.out.println(finalArray);
+    }
+
+
+}
---
+++ FanapCamps/Comparision/comparison2Array.java
@@ -0,0 +1,26 @@
+package FanapCamps.Comparision;
+
+import java.util.ArrayList;
+
+public interface comparison2Array {
+    public static ArrayList<Integer> compare(ArrayList<Integer> myList1, ArrayList<Integer> myList2) {
+        ArrayList<Integer> finalArray = new ArrayList<>();
+
+        if (myList1.size() < myList2.size()) {
+            for (int i = 0; i < myList1.size() ; i++) {
+                int e = myList1.get(i) + myList2.get(i);
+                finalArray.add(e);
+                System.out.println(e);
+            }
+        } else {
+            for (int i = 0; i < myList2.size() ; i++) {
+                int e = myList1.get(i) + myList2.get(i);
+                finalArray.add(e);
+
+                System.out.println(e);
+            }
+        }
+
+        return finalArray;
+    }
+}
