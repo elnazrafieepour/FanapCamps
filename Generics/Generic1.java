61d2d86b51e09d201e198e05f85e3516d820a147
admin <e.rafieepour@gmail.com>
2024-08-22 18:04:49 +0330

    tamrin Generics1-1

--- Generics/Generic1.java
+++ Generics/Generic1.java
@@ -3,20 +3,47 @@ package Generics;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Generic1<T> {
-    private T Var;
-    public void setVar(T Var){this.Var=Var;}
-    public T getVar(){return Var;}
+public class Generic1<E> {
 
 
+    public static List<Integer> addList(List<Integer> l1, List<Integer> l2) {
+        List<Integer> maxArray = (l1.size() < l2.size()) ? l2 : l1;
+        List<Integer> minArray = (l1.size() < l2.size()) ? l1 : l2;
 
-
-    public static void addList(List l1, List l2){
-        for (Object item: l1){
-           l1.addAll(l2);
-            System.out.println(l1);
-
+        for (int i = 0; i < minArray.size(); i++) {
+            maxArray.set(i, maxArray.get(i) + minArray.get(i));
         }
+        return maxArray;
+
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> a = new ArrayList<>();
+        a.add(3);
+        a.add(5);
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        a.add(1);
+
+        List<Integer> b = new ArrayList<>();
+        b.add(2);
+        b.add(1);
+        b.add(9);
+        b.add(7);
+        b.add(2);
+        b.add(4);
+        b.add(6);
+
+        System.out.println(addList(a,b));
+
 
     }
 }
