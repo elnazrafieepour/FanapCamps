87ff7ea3e3820a92ce6b0e08751ba1801ccafe12
admin <e.rafieepour@gmail.com>
2024-10-04 23:06:22 +0330

    Practice University 1- add grade to score of Student and add & Delete Course to Student and Professor

--- university/entity/Professor.java
+++ university/entity/Professor.java
@@ -1,11 +1,12 @@
 package university.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Professor extends Person {
     private String professorId;
     private Level level;
-    private List<Course> courseList;
+    private List<Course> courseList=new ArrayList<>();
 
     public Professor(String firstName, String lastName, String nationalCode, String entranceYear, String professorId, Level level, List<Course> courseList) {
         super(firstName, lastName, nationalCode, entranceYear);
@@ -39,6 +40,39 @@ public class Professor extends Person {
 
     public void setCourseList(List<Course> courseList) {
         this.courseList = courseList;
+    }
+
+    //Add Course List to Professor:
+    public static List<Course> AddCourseToProfessor(Professor professor, Course course){
+        List<Course> courseList= professor.getCourseList();
+        courseList.add(course);
+        return professor.getCourseList();
+    }
+
+    //Delete from Course List to Professor:
+    public static List<Course> DeleteCourseToProfessor(Professor professor, Course course){
+        List<Course> courseList= professor.getCourseList();
+        courseList.remove(course);
+        return professor.getCourseList();
+    }
+
+    public static void main(String[] args) {
+        //Update Field of Student:
+        Professor professor = new Professor("pro1", "rp", "1757070265", "1390");
+        Professor professor2 = new Professor("pro2", "rp2", "1757070265", "1390");
+
+        //AddCourseToStudent:
+        Course c1= new Course("ccccccc");
+        Course c2= new Course("ccccccc2");
+        Course c3= new Course("ccccccc3");
+        System.out.println(AddCourseToProfessor(professor, c1));
+        System.out.println(AddCourseToProfessor(professor, c2));
+        System.out.println(AddCourseToProfessor(professor, c3));
+        System.out.println(AddCourseToProfessor(professor2, c3));
+
+        //Delete from Course List to Student:
+        System.out.println("the Course is Deleted and the new courseList is: " + DeleteCourseToProfessor(professor, c1));
+
     }
 
     @Override
--- university/entity/Student.java
+++ university/entity/Student.java
@@ -1,13 +1,12 @@
 package university.entity;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Student extends Person {
     private String studentId;
     private Field field;
     private Faculty faculty;
-    private List<Course> courseList;
+    private List<Course> courseList = new ArrayList<>();
     private Map<Course, Grade> gradeMap;
 
     public Student(String firstName, String lastName, String nationalCode, String entranceYear, String studentId, Field field, Faculty faculty, List<Course> courseList, Map<Course, Grade> gradeMap) {
@@ -18,6 +17,7 @@ public class Student extends Person {
         this.courseList = courseList;
         this.gradeMap = gradeMap;
     }
+
     public Student(String firstName, String lastName, String nationalCode, String entranceYear, String studentId, Field field, Faculty faculty) {
         super(firstName, lastName, nationalCode, entranceYear);
         this.studentId = studentId;
@@ -66,8 +66,64 @@ public class Student extends Person {
         this.gradeMap = gradeMap;
     }
 
+    //Update Field of Student:
+    public static Student updateField(Student student, Field newField) {
+        student.setField(newField);
+        return student;
+    }
+
+    //Add Course List to Student:
+    public static List<Course> AddCourseToStudent(Student student, Course course) {
+        List<Course> courseList = student.getCourseList();
+        courseList.add(course);
+        return student.getCourseList();
+    }
+
+    //Delete from Course List to Student:
+    public static List<Course> DeleteCourseToStudent(Student student, Course course) {
+        List<Course> courseList = student.getCourseList();
+        courseList.remove(course);
+        return student.getCourseList();
+    }
+
+    //Add Score to Course of Student:
+    public static Map<Course, Grade> DefineScore(Course course, Grade grade) {
+        Map<Course, Grade> courseGradeMap = new HashMap<>();
+        courseGradeMap.put(course, grade);
+        return courseGradeMap;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        //Update Field of Student:
+        Student student = new Student("eli", "rp", "1757070265", "1390", "9060031007", new Field("math"), new Faculty("Science"));
+        Student student2 = new Student("eli2", "rp2", "1757070265", "1390", "9060031007", new Field("math"), new Faculty("Science"));
+        System.out.println(updateField(student, new Field("Physics")));
+        System.out.println(student2);
+
+        //AddCourseToStudent:
+        Course c1 = new Course("ccccccc");
+        Course c2 = new Course("ccccccc2");
+        Course c3 = new Course("ccccccc3");
+        System.out.println(AddCourseToStudent(student, c1));
+        System.out.println(AddCourseToStudent(student, c2));
+        System.out.println(AddCourseToStudent(student, c3));
+
+        //Delete from Course List to Student:
+        System.out.println("the Course is Deleted and the new courseList is: " + DeleteCourseToStudent(student, c1));
+
+        //Add Score to Course for Student:
+        System.out.println("please Input Your Index Course to Add Grade: ");
+        int courseIndx= scanner.nextInt();
+        System.out.println("the score is added to course of Student and this is: " + DefineScore(student.getCourseList().get(courseIndx), Grade.A));
+
+
+    }
+
     @Override
     public String toString() {
-        return "Student Name: " + this.getFirstName() + ", StudentId: " + this.studentId;
+        return "Student Name: " + this.getFirstName() + ", StudentId: " + this.studentId + ", Field: " + this.field.getName();
     }
 }
