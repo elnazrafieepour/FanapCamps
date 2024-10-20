15065f3b30f47932dd2a92c5ee4f8dad4784fb57
admin <e.rafieepour@gmail.com>
2024-10-20 22:30:08 +0330

    Keshtiban-Exc11.java

--- Keshtiban/Exc1.java
+++ Keshtiban/Exc1.java
@@ -4,21 +4,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exc1 {
-public static int[][] updateMtr(int[][] mtr) {
-    int size = mtr.length;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    public static int[][] updateMtr(int[][] mtr) {
+        int size = mtr.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
 
-            if (i - 1 >= 0 || j - 1 >= 0 || i + 1 < size || j + 1 < size) {
-                int neighbor = mtr[i][j - 1] + mtr[i][j + 1] + mtr[i - 1][j] + mtr[i + 1][j] + mtr[i - 1][j - 1] + mtr[i - 1][j + 1] + mtr[i + 1][j - 1] + mtr[i + 1][j = 1];
+                int neighbor = countOfLifes(mtr, i, j);
 
                 if (mtr[i][j] == 1) {
-                    if (neighbor < 2) {
+                    if (neighbor < 2 || neighbor > 3) {
                         mtr[i][j] = 0;
-                    } else if (neighbor >= 2) {
+                    } else {
                         mtr[i][j] = 1;
-                    } else if (neighbor > 3) {
-                        mtr[i][j] = 0;
                     }
 
                 } else {
@@ -31,26 +28,43 @@ public static int[][] updateMtr(int[][] mtr) {
 
             }
         }
+        return mtr;
     }
-    return mtr;
-}
+
+    public static int countOfLifes(int[][] mtr, int x, int y) {
+        int lifeCount = 0;
+        int size = mtr.length;
+        int[] directions = {1, 0, -1};
+        for (int i : directions) {
+            for (int j : directions) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                int newX = x + i;
+                int newY = y + j;
+                if (newX >= 0 && newX < size && newY >= 0 && newY < size) {
+                    lifeCount += mtr[newX][newY];
+                }
+            }
+        }
+        return lifeCount;
+    }
 
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please Input the size of Matrix: ");
         int input = scanner.nextInt();
         System.out.println("Please Input data for Matrix: ");
-        int[][] Mtr =new int[input][input];
-        for (int i= 0; i< input; i++){
-            for (int j=0; j< input; j++){
+        int[][] Mtr = new int[input][input];
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j < input; j++) {
                 Mtr[i][j] = scanner.nextInt();
             }
         }
         System.out.println("ypur matrix is: " + Arrays.deepToString(Mtr));
         updateMtr(Mtr);
         System.out.println("your new matrix is: " + Arrays.deepToString(Mtr));
-
-
+        scanner.close();
 
     }
 }
